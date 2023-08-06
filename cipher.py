9203eaa5303cb61579eed48719a7d816c8c1efa1
Ricky Nelakurti <rickyreadyn@gmail.com>
2023-08-06 15:40:55 -0400

    final_cypher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,16 @@
 # add your code here
+
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shifted_alphabet = 'fghijklmnopqrstuvwxyzabcde'
+
+encrypted_sentence = ''
+
+for char in sentence:
+    if char in alphabet:
+        index = alphabet.index(char)
+        encrypted_sentence += shifted_alphabet[index]
+    else:
+        encrypted_sentence += char
+print("The encrypted sentence is:", encrypted_sentence)
